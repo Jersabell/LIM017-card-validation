956a844d9f2beb7a0e00f6294406a8c02fc4a879
Jersabell <jersabell@gmail.com>
2022-02-22 04:11:24 -0500

    Primero

--- src/index.js
+++ src/index.js
@@ -1,3 +1,72 @@
-import validator from './validator.js';
+import validator from "./validator.js";
 
-console.log(validator);
+
+//Declarando constantes a utilizar
+    const divdeBancaInternet = document.querySelector("#BancaporInternet");
+    const main = document.getElementById("main");
+    const botonbancaInternet = document.querySelector("#botonbancaInternet");
+    const sendBotton = document.getElementById("sendBotton");
+    const divdeUltimaPagina = document.getElementById("resultadoBancaporInternet");
+    const resultadodeIsValid= document.getElementById("resultado");
+    const numeroEncriptado = document.getElementById("encriptado");
+    const againCard = document.getElementById("again");
+    const continuarCuenta = document.getElementById("continuarCuenta");
+    const inicioHeader= document.getElementById("irInicio")
+
+
+// Clickear el boton de Banca por Internet
+    botonbancaInternet.addEventListener("click", () => {
+        main.setAttribute("hidden", true);
+        divdeBancaInternet.removeAttribute("hidden");
+        inicioHeader.removeAttribute("hidden")
+      })
+
+// Función para enviar el número de tarjeta
+    sendBotton.addEventListener("click", () => { getNumber();
+     });
+
+// Función para obetener el número de la tarjeta de crédito
+    function getNumber() {
+       let numberCard = document.getElementById("numCard").value;
+       if (numberCard.length < 10) {
+            alert("El número de tarjeta tiene que tener 10 caracteres como mínimo");
+          } 
+       else if (numberCard.length > 16) {
+            alert("El número de tarjeta es de 16 caracteres como máximo");
+          } 
+       else {
+            divdeBancaInternet.setAttribute("hidden",true)
+            divdeUltimaPagina.removeAttribute("hidden");
+            
+            if (validator.isValid(numberCard)){
+                numeroEncriptado.innerHTML= validator.maskify(numberCard) 
+                resultadodeIsValid.innerHTML = "Número de Tarjeta Válido"
+                continuarCuenta.removeAttribute("hidden")
+                againCard.setAttribute("hidden",false)
+
+               }
+            else{
+                resultadodeIsValid.innerHTML = (numberCard + "<br>Número de tarjeta Inválido").fontcolor("#f80000")
+                againCard.removeAttribute("hidden")
+                continuarCuenta.setAttribute("hidden", false)
+               }        
+          }
+       return numberCard.value;
+    }
+
+// Intentar nuevamente    
+   againCard.addEventListener("click", () =>{
+      let numberCard = document.getElementById("numCard")
+      divdeUltimaPagina.setAttribute("hidden", true)
+      divdeBancaInternet.removeAttribute("hidden")
+      numberCard.value=""
+      
+      });
+
+// clickear CONTINUAR A CUENTA
+    continuarCuenta.addEventListener("click", ()=>{
+        window.history.back();
+    })
+
+
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,101 @@
 const validator = {
   // ...
-};
+ isValid: function (creditCardNumber){
+   
+     //convirtiendo a un Array
+     const convertToArray = Array.from(creditCardNumber);
+     console.log(convertToArray);
+     
+     //colocando numeros en orden inverso
+     const invertArray = convertToArray.reverse();
+     console.log(invertArray);
 
+     //función para seleccionar impares (pares para Lunh)
+     let impar =[]
+     let par =[]
+     
+     function selectImpar (){      
+        for (let i=0; i<invertArray.length;i++){
+            if(i %2 ==0){
+               par.push(Number(invertArray[i]))
+            }
+            else{
+               impar.push(Number(invertArray[i]))
+            }
+        }
+        return impar
+      }
+     selectImpar();
+     console.log(impar);
+     console.log(par)
+ 
+     // Multiplicar por 2 y sumar cifras si la multiplicaión es mayor a 10.
+     function operacionesDeImpares(){
+        let listo1 =[]
+        let listo2 = []
+        for(let i=0; i<impar.length;i++){
+            let multiplicar = impar[i]*2
+            if (multiplicar>=10){
+               let cifrasSumadas = 1 + (multiplicar%10)
+                  listo1.push(cifrasSumadas);
+                }
+            else{
+               let cifrasSinSumar= multiplicar
+               listo2.push(cifrasSinSumar)
+                }
+          }
+        let operaciones = listo1.concat(listo2);
+
+        return operaciones
+      }
+     let operacionesImparesHechas= operacionesDeImpares();
+     console.log(operacionesImparesHechas);
+     
+     // sumar todo el array y dividirlo entre 10. Si su residuo = 10, la tarjeta será valida
+     const arrrayUnido = operacionesImparesHechas.concat(par);
+     console.log(arrrayUnido);
+
+     function sumaTotaldelArray(){
+        let listoParaDividir = 0
+        for(let i=0; i<arrrayUnido.length;i++){
+            listoParaDividir+=arrrayUnido[i];
+              }
+        return listoParaDividir
+        }
+     const dividisionfinal= sumaTotaldelArray();
+     console.log(dividisionfinal);
+
+     function division(){
+        if(dividisionfinal%10==0){
+            return true
+        }
+        else{
+            return false
+        }
+      }
+     const resultado =division();
+     console.log(resultado)
+     return resultado
+  
+  },
+ 
+
+ maskify: function(creditCardNumber){
+
+  function ocultarEnMichis(){
+       let numerosOcultos= []
+       for (let i=0; i<creditCardNumber.length; i++){
+            if (i<creditCardNumber.length-4){
+               numerosOcultos.push("#");
+               }
+           else{
+               numerosOcultos.push(creditCardNumber[i])
+               }
+          }
+      return numerosOcultos.join("")
+  }
+ const verResultado = ocultarEnMichis();
+ console.log(verResultado);
+ return verResultado
+} }  
 export default validator;
